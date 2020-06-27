00c7a6fde17d0e3d3cd6e646969866e7abc9a700
holesome <66308006+holesome@users.noreply.github.com>
2020-06-27 16:32:19 +0200

    Update toggle-touchpad.sh

--- toggle-touchpad.sh
+++ toggle-touchpad.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
-# Toggle touchpad status
-# Using libinput and xinput
 
-# Use xinput list and do a search for touchpads. Then get the first one and get its name.
-device="$(xinput list | grep -P '(?<= )[\w\s:]*(?i)(touchpad|synaptics)(?-i).*?(?=\s*id)' -o | head -n1)"
+# Toggle touchpad status (enable/disable)
+# Using libinput and xinput. This script depends on them.
 
-# If it was activated disable it and if it wasn't disable it
+# Original script: https://github.com/lahwaacz/Scripts/blob/master/toggle-touchpad.sh.
+# Forked and modified by https://github.com/holesome to make the script POSIX compliant, which helps in portability and efficiency.
+# The first grep command got also modified to make it simpler because the original script didn't work on my setup.
+
+# Getting the name of the device. First list the devices with xinput list and then filter the result to only include the full name of the device.
+device=`xinput list | grep -e '[A-Za-z0-9]\+.*TouchPad' -o`
+
+# If it was enabled, disable it. And if it was disabled, enable it
 [[ "$(xinput list-props "$device" | grep -P ".*Device Enabled.*\K.(?=$)" -o)" == "1" ]] &&
     xinput disable "$device" ||
     xinput enable "$device"
